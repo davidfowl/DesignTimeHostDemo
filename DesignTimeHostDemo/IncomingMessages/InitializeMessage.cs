fb9e21a59477f35ee290cf524288c4ad79fd3fdd
Troy Dai <trdai@microsoft.com>
2014-10-23 16:05:27 -0700

    Update the design time host demo
    1. Update the incoming and outgoing message based on the latest KRuntime
    
    Note: The ComplicationOptionsMessage is not migrated since it takes
    dependencies on Rosyln
    
    2. Stop redirect the host process's output to the main program console
    because the original redirect make the debugging very difficult.

--- DesignTimeHostDemo/IncomingMessages/InitializeMessage.cs
+++ DesignTimeHostDemo/IncomingMessages/InitializeMessage.cs
@@ -5,7 +5,7 @@ namespace Microsoft.Framework.DesignTimeHost.Models.IncomingMessages
 {
     public class InitializeMessage
     {
-        public string TargetFramework { get; set; }
+        public string Configuration { get; set; }
         public string ProjectFolder { get; set; }
     }
 }
